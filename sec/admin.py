2885b4d82afd44d9a84fcfbb4f5c99cbacf5d335
lotan1 <lotanro@gmail.com>
2013-12-31 22:54:11 +0200

    tango 6 done

--- sec/admin.py
+++ sec/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from sec.models import Category, Page, Stam
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Page)
+admin.site.register(Stam)
