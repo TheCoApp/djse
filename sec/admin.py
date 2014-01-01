46c72b98db418ba5bda0461d216ca408ff0d302f
lotan1 <lotanro@gmail.com>
2014-01-02 00:13:38 +0200

    ad perek 8

--- sec/admin.py
+++ sec/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from sec.models import Category, Page, Stam
+from sec.models import Category, Page, Stam, UserProfile
+
 
 # Register your models here.
 
 admin.site.register(Category)
 admin.site.register(Page)
 admin.site.register(Stam)
+admin.site.register(UserProfile)
